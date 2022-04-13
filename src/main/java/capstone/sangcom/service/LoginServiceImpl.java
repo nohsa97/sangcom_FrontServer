2e270051fbf4fa655729ffe79b009466c43ad39f
happiestlife <chickenman10@naver.com>
2022-04-13 21:43:12 +0900

    modify : 로그 삭제

--- src/main/java/capstone/sangcom/service/LoginServiceImpl.java
+++ src/main/java/capstone/sangcom/service/LoginServiceImpl.java
@@ -8,7 +8,6 @@ import capstone.sangcom.response.login.LoginResponse;
 import capstone.sangcom.response.login.SuccessResponse;
 import capstone.sangcom.response.login.TokenResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONException;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -37,15 +36,12 @@ public class LoginServiceImpl implements LoginService{
                 .build()
                 .toUri();
 
-        log.info(String.valueOf(uri));
-
         RequestEntity<LoginDTO> requestEntity = RequestEntity
                 .post(uri)
                 .contentType(MediaType.APPLICATION_JSON)
                 .header("authorization","null")
                 .body(loginInfo);
 
-        log.info("sending");
         //헤더를 함께 보낼때는 exchange메서드를 사용한다 파라미터 1 요청정보들이 들어있는 entity, 2 응답받을 타입
         ResponseEntity<LoginResponse> response = restTemplate.exchange(requestEntity, LoginResponse.class);
 
@@ -61,15 +57,12 @@ public class LoginServiceImpl implements LoginService{
                 .build()
                 .toUri();
 
-        log.info(String.valueOf(uri));
-
         RequestEntity<Void> requestEntity = RequestEntity
                 .get(uri)
                 .accept(MediaType.APPLICATION_JSON)
                 .header("authorization",access)
                 .build();
 
-        log.info("sending");
         //헤더를 함께 보낼때는 exchange메서드를 사용한다 파라미터 1 요청정보들이 들어있는 entity, 2 응답받을 타입
         ResponseEntity<SuccessResponse> response = restTemplate.exchange(requestEntity, SuccessResponse.class);
 
@@ -77,7 +70,7 @@ public class LoginServiceImpl implements LoginService{
     }
 
     @Override
-    public String getAccessToken(String refresh) throws JSONException {
+    public String getAccessToken(String refresh){
         URI uri = UriComponentsBuilder
                 .fromUriString("http://localhost:3000")
                 .path("/api/auth/refresh")
@@ -85,15 +78,12 @@ public class LoginServiceImpl implements LoginService{
                 .build()
                 .toUri();
 
-        log.info(String.valueOf(uri));
-
         RequestEntity<RefreshRequest> requestEntity = RequestEntity
                 .post(uri)
                 .contentType(MediaType.APPLICATION_JSON)
                 .header("authorization","null")
                 .body(new RefreshRequest(refresh));
 
-        log.info("sending");
         //헤더를 함께 보낼때는 exchange메서드를 사용한다 파라미터 1 요청정보들이 들어있는 entity, 2 응답받을 타입
         ResponseEntity<TokenResponse> response = restTemplate.exchange(requestEntity, TokenResponse.class);
 
@@ -109,7 +99,6 @@ public class LoginServiceImpl implements LoginService{
                 .build()
                 .toUri();
 
-        log.info(String.valueOf(uri));
 
         RequestEntity<FindPasswordDTO> requestEntity = RequestEntity
                 .post(uri)
@@ -117,7 +106,6 @@ public class LoginServiceImpl implements LoginService{
                 .header("authorization","null")
                 .body(findPasswordDTO);
 
-        log.info("sending");
         //헤더를 함께 보낼때는 exchange메서드를 사용한다 파라미터 1 요청정보들이 들어있는 entity, 2 응답받을 타입
         ResponseEntity<SuccessResponse> response = restTemplate.exchange(requestEntity, SuccessResponse.class);
 
@@ -133,15 +121,12 @@ public class LoginServiceImpl implements LoginService{
                 .build()
                 .toUri();
 
-        log.info(String.valueOf(uri));
-
         RequestEntity<RegisterDTO> requestEntity = RequestEntity
                 .post(uri)
                 .contentType(MediaType.APPLICATION_JSON)
                 .header("authorization","null")
                 .body(registerDTO);
 
-        log.info("sending");
         //헤더를 함께 보낼때는 exchange메서드를 사용한다 파라미터 1 요청정보들이 들어있는 entity, 2 응답받을 타입
         ResponseEntity<SuccessResponse> response = restTemplate.exchange(requestEntity, SuccessResponse.class);
 
