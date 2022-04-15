d6c11cd7c24eaa1a772a7c7834399d343c36c2fc
happiestlife <chickenman10@naver.com>
2022-04-15 13:01:12 +0900

    refactor : 서비스 계층 api 서버와 통신 중복 코드 template/callback 패턴으로 리모델링 new : 로그아웃 기능 추가

--- src/main/java/capstone/sangcom/service/LoginServiceImpl.java
+++ src/main/java/capstone/sangcom/service/LoginServiceImpl.java
@@ -7,131 +7,57 @@ import capstone.sangcom.request.login.RefreshRequest;
 import capstone.sangcom.response.login.LoginResponse;
 import capstone.sangcom.response.login.SuccessResponse;
 import capstone.sangcom.response.login.TokenResponse;
+import capstone.sangcom.template.ApiTemplate;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
 
 @Slf4j
 @Service
 public class LoginServiceImpl implements LoginService{
 
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+    private final ApiTemplate apiTemplate;
 
-    public LoginServiceImpl(RestTemplate restTemplate){
+    public LoginServiceImpl(RestTemplate restTemplate, ApiTemplate apiTemplate){
         this.restTemplate = restTemplate;
+        this.apiTemplate = apiTemplate;
     }
 
     @Override
     public LoginResponse login(LoginDTO loginInfo){
-        URI uri = UriComponentsBuilder
-                .fromUriString("http://localhost:3000")
-                .path("/api/user/login")
-                .encode()
-                .build()
-                .toUri();
-
-        RequestEntity<LoginDTO> requestEntity = RequestEntity
-                .post(uri)
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("authorization","null")
-                .body(loginInfo);
-
-        //헤더를 함께 보낼때는 exchange메서드를 사용한다 파라미터 1 요청정보들이 들어있는 entity, 2 응답받을 타입
-        ResponseEntity<LoginResponse> response = restTemplate.exchange(requestEntity, LoginResponse.class);
-
-        return response.getBody();
+        LoginResponse result = apiTemplate.postExecute("/user/login", loginInfo, "null", LoginResponse.class);
+        System.out.println(result.isSuccess());
+        return result;
     }
 
     @Override
     public boolean checkLogin(String access, String refresh){
-        URI uri = UriComponentsBuilder
-                .fromUriString("http://localhost:3000")
-                .path("/api/user/login/check")
-                .encode()
-                .build()
-                .toUri();
+        SuccessResponse result = apiTemplate.getExecute("/user/login/check", access, SuccessResponse.class);
 
-        RequestEntity<Void> requestEntity = RequestEntity
-                .get(uri)
-                .accept(MediaType.APPLICATION_JSON)
-                .header("authorization",access)
-                .build();
-
-        //헤더를 함께 보낼때는 exchange메서드를 사용한다 파라미터 1 요청정보들이 들어있는 entity, 2 응답받을 타입
-        ResponseEntity<SuccessResponse> response = restTemplate.exchange(requestEntity, SuccessResponse.class);
-
-        return response.getBody().isSuccess();
+        return result.isSuccess();
     }
 
     @Override
     public String getAccessToken(String refresh){
-        URI uri = UriComponentsBuilder
-                .fromUriString("http://localhost:3000")
-                .path("/api/auth/refresh")
-                .encode()
-                .build()
-                .toUri();
+        TokenResponse result = apiTemplate.postExecute("/auth/refresh", new RefreshRequest(refresh), "null", TokenResponse.class);
 
-        RequestEntity<RefreshRequest> requestEntity = RequestEntity
-                .post(uri)
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("authorization","null")
-                .body(new RefreshRequest(refresh));
-
-        //헤더를 함께 보낼때는 exchange메서드를 사용한다 파라미터 1 요청정보들이 들어있는 entity, 2 응답받을 타입
-        ResponseEntity<TokenResponse> response = restTemplate.exchange(requestEntity, TokenResponse.class);
-
-        return response.getBody().getAccess_token();
+        return result.getAccess_token();
     }
 
     @Override
     public boolean findPassword(FindPasswordDTO findPasswordDTO) {
-        URI uri = UriComponentsBuilder
-                .fromUriString("http://localhost:3000")
-                .path("/api/user/password/find")
-                .encode()
-                .build()
-                .toUri();
 
+        SuccessResponse result = apiTemplate.postExecute("/user/password/find", findPasswordDTO, "null", SuccessResponse.class);
 
-        RequestEntity<FindPasswordDTO> requestEntity = RequestEntity
-                .post(uri)
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("authorization","null")
-                .body(findPasswordDTO);
-
-        //헤더를 함께 보낼때는 exchange메서드를 사용한다 파라미터 1 요청정보들이 들어있는 entity, 2 응답받을 타입
-        ResponseEntity<SuccessResponse> response = restTemplate.exchange(requestEntity, SuccessResponse.class);
-
-        return response.getBody().isSuccess();
+        return result.isSuccess();
     }
 
     @Override
     public boolean register(RegisterDTO registerDTO) {
-        URI uri = UriComponentsBuilder
-                .fromUriString("http://localhost:3000")
-                .path("/api/user/register")
-                .encode()
-                .build()
-                .toUri();
+        SuccessResponse result = apiTemplate.postExecute("/user/register", registerDTO, "null", SuccessResponse.class);
 
-        RequestEntity<RegisterDTO> requestEntity = RequestEntity
-                .post(uri)
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("authorization","null")
-                .body(registerDTO);
-
-        //헤더를 함께 보낼때는 exchange메서드를 사용한다 파라미터 1 요청정보들이 들어있는 entity, 2 응답받을 타입
-        ResponseEntity<SuccessResponse> response = restTemplate.exchange(requestEntity, SuccessResponse.class);
-
-        System.out.println(response.getBody().isSuccess());
-
-        return response.getBody().isSuccess();
+        return result.isSuccess();
     }
 }
---
+++ src/main/java/capstone/sangcom/template/ApiTemplate.java
@@ -0,0 +1,65 @@
+package capstone.sangcom.template;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Component
+@Slf4j
+public class ApiTemplate {
+
+    private final RestTemplate restTemplate;
+    private static final String post = "http://localhost:3000";
+
+    public ApiTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public <P> P getExecute(String detailUri, String token, Class<P> cls){
+        URI uri = UriComponentsBuilder
+                .fromUriString(post)
+                .path("/api" + detailUri)
+                .build()
+                .toUri();
+
+        log.info("GET {}",String.valueOf(uri));
+
+        RequestEntity<Void> requestEntity = RequestEntity
+                .get(uri)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("authorization",token)
+                .build();
+
+        //헤더를 함께 보낼때는 exchange메서드를 사용한다 파라미터 1 요청정보들이 들어있는 entity, 2 응답받을 타입
+        ResponseEntity<P> response = restTemplate.exchange(requestEntity, cls);
+
+        return response.getBody();
+    }
+
+    public <Q, P> P postExecute(String detailUri, Q data, String token, Class<P> cls){
+        URI uri = UriComponentsBuilder
+                .fromUriString(post)
+                .path("/api" + detailUri)
+                .build()
+                .toUri();
+
+        log.info("POST {}",String.valueOf(uri));
+
+        RequestEntity<Q> requestEntity = RequestEntity
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("authorization",token)
+                .body(data);
+
+        //헤더를 함께 보낼때는 exchange메서드를 사용한다 파라미터 1 요청정보들이 들어있는 entity, 2 응답받을 타입
+        ResponseEntity<P> response = restTemplate.exchange(requestEntity, cls);
+
+        return response.getBody();
+    }
+}
