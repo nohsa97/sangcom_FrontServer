21f9c13957a444c2eb42250a8fa904969aa04f44
nohsa97 <81568105+nohsa97@users.noreply.github.com>
2022-04-15 17:59:43 +0900

    update:게시판목록
    네

--- src/main/java/capstone/sangcom/service/login/LoginService.java
+++ src/main/java/capstone/sangcom/service/login/LoginService.java
@@ -4,12 +4,12 @@ import capstone.sangcom.dto.login.FindPasswordDTO;
 import capstone.sangcom.dto.login.LoginDTO;
 import capstone.sangcom.dto.login.RegisterDTO;
 import capstone.sangcom.response.login.LoginResponse;
-import org.json.JSONException;
+
 
 public interface LoginService {
     public LoginResponse login(LoginDTO loginInfo);
     public boolean checkLogin(String access, String refresh);
-    public String getAccessToken(String refresh) throws JSONException;
+    public String getAccessToken(String refresh);
     public boolean findPassword(FindPasswordDTO findPasswordDTO);
     public boolean register(RegisterDTO registerDTO);
 }
